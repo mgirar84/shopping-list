219486c1ce29935f71b254c0216c9cc594100a3b
Milo Girardet <milo.girardet@playsportsnetwork.com>
2024-01-01 13:31:09 +0000

    setup heading component

---
+++ src/components/atoms/Heading/index.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+import { HeadingProps } from "./types";
+
+export const StyledHeading = styled.h1<HeadingProps>`
+  color: #333;
+  font-size: 24px;
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
-import { ShoppingForm, ShoppingList } from "@/components/molecules";
+import { useState } from "react";
+
 import styled from "styled-components";
+
+import { StyledHeading } from "../components/atoms";
+import { ShoppingForm, ShoppingList } from "../components/molecules";
 
 const StyledMain = styled.main`
   max-width: 1350px;
@@ -13,12 +17,15 @@ const Title = styled.h1`
   color: #333;
 `;
 
-const Main = () => (
-  <StyledMain>
-    <Title>Shopping List</Title>
-    <ShoppingForm onSubmit={() => {}} />
-    <ShoppingList items={[{ name: "item", id: "1" }]} />
-  </StyledMain>
-);
+const Main = () => {
+  const [items, setItems] = useState();
+  return (
+    <StyledMain>
+      <StyledHeading>Shopping List</StyledHeading>
+      <ShoppingForm onSubmit={() => {}} />
+      <ShoppingList items={[{ name: "item", id: "1" }]} />
+    </StyledMain>
+  );
+};
 
 export default Main;
