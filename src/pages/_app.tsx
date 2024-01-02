19a19b2da441d1cd72dd479b16ceabd41efcc7a9
Milo Girardet <milo.girardet@playsportsnetwork.com>
2024-01-02 12:25:34 +0000

    update app and document to latest versions from next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import { FC } from "react";
 import type { AppProps } from "next/app";
 import GlobalStyle from '../global-styles'
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,59 +1,15 @@
-import React from 'react';
-import Document, {
-  Head,
-  Main,
-  NextScript,
-  Html,
-  DocumentContext,
-} from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
+import { Html, Head, Main, NextScript } from "next/document";
 
-interface Props {
-  styles: JSX.Element;
-  html: string;
-  head?: (JSX.Element | null)[] | undefined;
-}
+const Document = () => {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
 
-export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext): Promise<Props> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () => originalRenderPage({
-        enhanceApp: (App: any) => (props: any) => (
-          sheet.collectStyles(<App {...props} />)
-        ),
-      });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
-
-  render(): JSX.Element {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://use.typekit.net/ddk6nfu.css" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
+export default Document;
