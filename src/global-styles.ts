8d6d8936e517781f06c9d84abd6d3c79a0b779e2
Milo Girardet <milo.girardet@playsportsnetwork.com>
2024-01-02 11:13:08 +0000

    move font to global styl sheet

--- src/global-styles.ts
+++ src/global-styles.ts
@@ -4,11 +4,7 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
-  }
-
-  body {
-    margin: 0;
-    padding: 0;
+    font-family: "Arial", sans-serif;
   }
 `;
 
