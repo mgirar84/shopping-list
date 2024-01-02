898e034000e68f0f08ebd55c3555e5d7422b5bee
Milo Girardet <milo.girardet@playsportsnetwork.com>
2024-01-02 11:34:41 +0000

    update docblock, add return type

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,10 +1,10 @@
 /**
  * Check if the given string contains only letters and spaces
  *
- * @param {string} itemName the string to check
+ * @param {string} itemName the item to check
  * @returns {boolean} true if the item is valid
  */
-export const isItemValid = (itemName: string) => {
+export const isItemValid = (itemName: string): boolean => {
   const textRegex = /^[a-zA-Z\s]+$/;
   return textRegex.test(itemName);
 };
